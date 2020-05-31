1e70f148ae3add4039a2a81d998febb426699ea5
kosthon <alejofova@gmail.com>
2020-05-31 15:13:01 -0500

    Vista nueva de Videos

--- Front/src/components/BodyVideos.vue
+++ Front/src/components/BodyVideos.vue
@@ -1,25 +1,34 @@
 <template>
   <div>
     <div class="contenedor">
-      <div class="video"></div>
-      <div class="descrip">
-        <div class="nombre"><h1>Programación Web</h1></div>
-        <div class="autor"><h5>Sergio Rubiano</h5></div>
+      <div class="todo">
+        <div class="video">
+          <iframe
+            width="800"
+            height="500"
+            src="https://www.youtube.com/embed/0ToMTAyrdVY"
+            frameborder="0"
+            allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+            allowfullscreen
+          ></iframe>
+        </div>
+        <div class="descrip">
+          <div class="nombre"><h1>Programación Web</h1></div>
+          <div class="autor"><h5>Sergio Rubiano</h5></div>
+        </div>
       </div>
     </div>
-    <div class="Mentor">
-      <div class="picture"></div>
-      <div class="DescripMentor">
-        <h1>Nombre</h1>
-        <h2>Carrera</h2>
-        <h3>Correo</h3>
-        <h3>Numero</h3>
+    <div class="full"></div>
+    <div class="mentor">
+      <div class="picture">
+        <img src="../assets/videos/Sergio.jpg" />
       </div>
-    </div>
-    <div class="comnetarios">
-      <ul>
-        <li>comentarios</li>
-      </ul>
+      <div class="descripmentor">
+        <h1>Mentor: Sergio Rubiano</h1>
+        <h2>Ingeniero de Sistemas</h2>
+        <h3>sergiorubiana@gmial.com</h3>
+        <h3>311 666 6666</h3>
+      </div>
     </div>
   </div>
 </template>
@@ -43,17 +52,53 @@ export default {};
 }
 .video {
   display: flex;
-  border: black solid 1px;
   width: 800px;
   height: 500px;
   margin-top: 50px;
 }
 .descrip {
-  border: black solid 1px;
   display: flex;
   flex-direction: column;
   font-family: "Lato", sans-serif;
   font-style: bold;
   font-size: 25px;
+  color: #001847;
+}
+.mentor {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  margin-top: 16px;
+}
+.picture {
+  margin-right: 16px;
+}
+.descripmentor {
+  margin-left: 16px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
+.descripmentor h1 {
+  font-family: "Lato", sans-serif;
+  font-style: bold;
+  font-size: 40px;
+  color: #001847;
+  margin-bottom: 8px;
+}
+.descripmentor h2 {
+  font-family: "Lato", sans-serif;
+  font-style: bold;
+  font-size: 24px;
+  color: #001847;
+}
+.descripmentor h3 {
+  font-family: "Lato", sans-serif;
+  font-style: bold;
+  font-size: 16px;
+  color: #001847;
+}
+.picture img {
+  border-radius: 30% 70% 70% 30% / 30% 30% 70% 70%;
 }
 </style>
